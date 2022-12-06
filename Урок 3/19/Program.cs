edff9a9c0706ad088216ad45ecb33331ffb29450
Serek5 <119046209+Serek5@users.noreply.github.com>
2022-12-06 17:47:50 +0700

    Update Program.cs

--- Урок 3/19/Program.cs
+++ Урок 3/19/Program.cs
@@ -2,7 +2,8 @@
 Console.Write("Введите число: ");
 string number = Console.ReadLine();
 
-void Number1(string number){
+void Number1(string number)
+{
   if (number[0]==number[4] & number[1]==number[3])
     Console.WriteLine($"Число: {number} - палиндром.");  
   else Console.WriteLine($"Число: {number} - не палиндром.");
