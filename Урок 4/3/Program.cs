0972b77e79d59bd9fb6bb0c912da10cd422bcbe0
Serek5 <Serek1456@gmail.com>
2022-12-07 21:06:46 +0700

    4 Дом.Раб Готова

--- Урок 4/3/Program.cs
+++ Урок 4/3/Program.cs
@@ -2,14 +2,15 @@
 
 int [] numbers = new int[8];
 
-for (int i = 0; i < numbers.Length; i++)
- {
-    numbers [i] = new Random().Next(0, 11);
-    Console.Write(" " + Method (i) + " ");
- }
-
 int Method (int a)
 {
     return numbers[a];
 }
+Console.Write("[");
+for (int i = 0; i < numbers.Length; i++)
+ {   
+    numbers [i] = new Random().Next(0, 11);     
+    Console.Write(" " + Method (i) + " ");
+  }
+Console.Write("]");
 
