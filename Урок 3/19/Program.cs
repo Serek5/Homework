1b652f5ab11d56275252e9f496ec617d34080e04
Serek5 <Serek1456@gmail.com>
2022-12-07 20:28:33 +0700

    4 Дом.Раб Готова

--- Урок 3/19/Program.cs
+++ Урок 3/19/Program.cs
@@ -1,16 +1,15 @@
 ﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
-Console.Write("Введите число: ");
-string number = Console.ReadLine();
+// Console.Write("Введите число: ");
+// string number = Console.ReadLine();
 
-void Number1(string number){
-  if (number[0]==number[4] & number[1]==number[3])
-    Console.WriteLine($"Число: {number} - палиндром.");  
-  else Console.WriteLine($"Число: {number} - не палиндром.");
-}
-if (number.Length == 5)
-  Number1(number);
-else Console.WriteLine("Error, Введите пятизначное число.");
+// void Number1(string number){
+//   if (number[0]==number[4] & number[1]==number[3])
+//     Console.WriteLine($"Число: {number} - палиндром.");  
+//   else Console.WriteLine($"Число: {number} - не палиндром.");
+// }
+// if (number.Length == 5)
+//   Number1(number);
+// else Console.WriteLine("Error, Введите пятизначное число.");
 
 
- 		
 	
---
+++ Урок 4/1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+ Console.Write("Число 1: ");
+  int number1 = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Число 2: ");
+  int number2 = Convert.ToInt32(Console.ReadLine());
+
+int Degree(int number1, int number2)
+{
+  int result = 1;
+  for(int i=1; i <= number2; i++)
+      result = result * number1;
+         return result;
+}
+  int Degree1 = Degree(number1, number2);
+  Console.WriteLine("Ответ: " + Degree1);
