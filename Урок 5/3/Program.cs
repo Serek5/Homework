cc36513f7612ed070be8b877edf22a642a8d42fd
Serek5 <Serek1456@gmail.com>
2022-12-11 23:28:04 +0700

    11.12.2022

--- Урок 5/3/Program.cs
+++ Урок 5/3/Program.cs
@@ -6,7 +6,7 @@ int[] array = new int[length];
 Random random = new Random();
 for (int i = 0; i < length; i++)
 {
-    array[i] = random.Next(0,1000);
+    array[i] = random.Next(0,10);
 }
 int DiffMaxMin(int[] array)
 {
